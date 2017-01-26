5e29bf245bc907a2d6af8e6df88e988b17f3abdb
Xavi Serra <xaviserrag@gmail.com>
2017-01-26 01:57:59 -0800

    Added the call to get random movies from themoviedb

--- src/reducers/movies.js
+++ src/reducers/movies.js
@@ -1,23 +1,29 @@
 // import { createStore } from 'react-redux';
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
 const defaultState = {
   movies: [],
   movieIndex: 0
 };
 
+const totalPages = 1000;
+const itemsPerPage = 20;
+
 const getMovie = () => {
   let xhr = new XMLHttpRequest();
-  xhr.open("GET", 'http://image.tmdb.org/t/p/discover', true);
+  let page = getRandomInt(0, totalPages);
+  let url = `https://api.themoviedb.org/3/discover/` + 
+  `movie?api_key=23564cb33be66abe06ab37d2417ce3b3&language=en-US& ` + 
+  `sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`
+  xhr.open("GET", url, true);
   xhr.setRequestHeader('Accept', 'application/json');
   xhr.responseType = "text";
   xhr.onreadystatechange = function () {
     if (this.readyState === 4) {
       if (this.status === 200){
-        if (typeof success === "function") {
-          console.log(JSON.parse(this.response));	
-        }else{
-          console.error('No success callback, but the request gave results')
-        }
+          let data = JSON.parse(this.response);
+          let randomItem = getRandomInt(0, itemsPerPage - 1);
+          console.log(data, randomItem, data.results[randomItem])
       }else{
         console.error('No error callback')
       }
