dd6338d0f4151349ced190e0a0bee11d1826c1cc
Xavi Serra <xaviserrag@gmail.com>
2017-01-28 23:53:57 -0800

    Add link to moviedb and prepared build process to ghpages

--- src/containers/Footer.js
+++ src/containers/Footer.js
@@ -3,7 +3,9 @@ import React from 'react';
 export default () => (
   <div className="App-footer">
     <div className="Moviesdb-logo">
-      <img src="themoviedb.png" alt="themoviedb logo"/>
+      <a href="https://www.themoviedb.org">
+        <img src="themoviedb.png" alt="themoviedb logo"/>
+      </a>
     </div>  
   </div>
 );
