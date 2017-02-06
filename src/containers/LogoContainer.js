dc92e96cc55f918d066ecff0c5aeaea464198adf
Xavi Serra <xaviserrag@gmail.com>
2017-02-05 21:41:50 -0800

    Created CNAME, added animation to logo

--- src/containers/LogoContainer.js
+++ src/containers/LogoContainer.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import '../css/Logo.css';
-const mapStateToProps = (state) => state.movies.movie || {};
+const mapStateToProps = (state) => state.movies || {};
 
 class Logo extends Component {
   render () {
     // If no movie in the state, the logo should be shown in the center
     // if there is a movie, the logo should go on top of the screen.
-    const hasMovie = this.props.title;
+    const hasMovie = this.props.movie.title;
     const logoClass = hasMovie ? 'logo-top' : 'logo-centered';
+
+    let logoAnimation = '';
+    if (this.props.isFetching) {
+      logoAnimation = 'logo-easeout';
+    }
 
     return (
       <div className={logoClass}>
         <span></span>
-        <img className="logo" src="logo.png" alt="watchit logo" />
+        <img className={logoAnimation} src="watchit-logo.png" alt="watchit logo" />
       </div>
     )
   }
