487855f8601aa7b25dadba36f3a94978c573baf7
Xavi Serra <xaviserrag@gmail.com>
2017-02-05 17:05:17 -0800

    Fixing styles, adding header

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import MovieContainer from './containers/MovieContainer';
 import FooterContainer from './containers/FooterContainer';
 import LogoContainer from './containers/LogoContainer';
+import Header from './components/Header';
 
 import './css/App.css';
 
@@ -11,6 +12,7 @@ class App extends Component {
 
     return (
       <div className="App">
+        <Header />
         <LogoContainer />
         <MovieContainer />
         <FooterContainer />
